512173c2bb272f660dceb483d02875dc0affb40e
chentyphoon <chentyphoon@gmail.com>
2016-01-07 22:05:32 +0800

    toolbar

--- app/src/main/java/com/example/kp2101/journey0100/JourneyAdd.java
+++ app/src/main/java/com/example/kp2101/journey0100/JourneyAdd.java
@@ -1,5 +1,6 @@
 package com.example.kp2101.journey0100;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -7,10 +8,15 @@ import android.support.v7.widget.Toolbar;
  * Created by KP2101 on 2016/1/7.
  */
 public class JourneyAdd  extends AppCompatActivity {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.journey_add);
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    getSupportActionBar().setTitle("test");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.journey_add);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("新增旅程");
+    }
 }
